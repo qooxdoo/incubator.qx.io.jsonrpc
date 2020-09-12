25853e86e7186d8718f1705a9159cc1b348f5c35
Christian Boulanger <info@bibliograph.org>
2020-09-12 12:14:03 +0200

    Doc fix

--- source/class/qx/io/transport/__init__.js
+++ source/class/qx/io/transport/__init__.js
@@ -5,11 +5,12 @@
  * <p>{@link qx.io.transport.ITransport} specifies that a
  * transport has to provide for three things:</p>
  * <ol>
- *   <li>It must have an "endpoint" property which identifies where the server is located</li>
- *   <li>It must have a "send" method which knows how to transport the which takes an UTF-8 encoded message string
- *      and passes it to the endpoint, and</li>
- *   <li>It must fire a "message" event when it receives such a message from the peer, regardless if
- *   this message is a normal "response" (like in a HTTP request) or an incoming message in a duplex
- *   communication channel.</li>
+ *   <li>It must have an "endpoint" property of type string which identifies where
+ *      the server is located</li>
+ *   <li>It must have a "send" method which knows how to deliver an UTF-8
+ *      encoded message string to the endpoint, and</li>
+ *   <li>It must fire a "message" event when it receives such a message from
+ *      the peer, regardless if this message is a normal "response" (like in a
+ *      HTTP request) or an incoming message in a duplex communication channel.</li>
  * </ol>
  */
